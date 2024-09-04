e8c1b22ce11d56f1a93a3b59c2d116d818402193
Nitish Kr <57797801+Nitish-web-nik@users.noreply.github.com>
2024-09-05 01:16:57 +0530

    Time: 68 ms (74.73%), Space: 37.5 MB (76.63%) - LeetHub

---
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        constexpr int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int ans = 0;
+        int d = 0;  // 0 := north, 1 := east, 2 := south, 3 := west
+        int x = 0;  // the start x
+        int y = 0;  // the start y
+        unordered_set<pair<int, int>, PairHash> obstaclesSet;
+
+        for (const vector<int>& o : obstacles)
+            obstaclesSet.insert({o[0], o[1]});
+
+        for (const int c : commands) {
+            if (c == -1) {
+                d = (d + 1) % 4;
+            } else if (c == -2) {
+                d = (d + 3) % 4;
+            } else {
+                for (int step = 0; step < c; ++step) {
+                    int nextX = x + dirs[d][0];
+                    int nextY = y + dirs[d][1];
+                    if (obstaclesSet.count({nextX, nextY}))
+                        break;
+                    x = nextX;
+                    y = nextY;
+                }
+            }
+            ans = max(ans, x * x + y * y);
+        }
+
+        return ans;
+    }
+
+private:
+    struct PairHash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
+};
