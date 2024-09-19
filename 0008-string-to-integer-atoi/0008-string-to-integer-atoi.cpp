a3f394268d5dfe19241596147e8c4d66792a105c
Nitish Kr <57797801+Nitish-web-nik@users.noreply.github.com>
2024-09-19 17:39:41 +0530

    Time: 3 ms (58.63%), Space: 9.1 MB (68.62%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+
+        //Step 1) Handling the issue of Whitespace
+        int n = s.length();
+        int i = 0;
+        int ans = 0;
+        int sign = 1;
+        while(i < n && s[i]==' ')
+        {
+            i++;
+        }
+
+        //Step  2) Handling the issue of Signedness
+        if(i < n && (s[i] == '+' || s[i] == '-'))
+        {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+        //Step 3) Conversion
+        while(i<n && isdigit(s[i]))
+        {
+            int digit = s[i] - '0';
+
+            //Handling the issue of overflow
+            if(ans > (INT_MAX - digit) / 10)
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            ans = ans*10 + digit;
+            i++;
+        }
+
+        return ans*sign;
+    }
+};
